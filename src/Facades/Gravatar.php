15116e0e2ef41d7aaa560214cf21f46f31ab840b
Sergey Danilchenko <s.danilchenko@ttbooking.ru>
2025-11-04 19:48:26 +0200

    Facade docblock

--- src/Facades/Gravatar.php
+++ src/Facades/Gravatar.php
@@ -3,7 +3,14 @@
 namespace LaravelGravatar\Facades;
 
 use Illuminate\Support\Facades\Facade;
+use LaravelGravatar\Image;
+use LaravelGravatar\Profile;
 
+/**
+ * @method static Image image(string|null $email = null, string|null $presetName = null)
+ * @method static Image avatar(string|null $email = null, string|null $presetName = null)
+ * @method static Profile profile(string|null $email = null, string|null $format = null)
+ */
 class Gravatar extends Facade
 {
     /**
